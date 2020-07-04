0caabd27271d95bd1a95e5f9d8d233cf8b0eb96b
Alexandre Giannetto <alexg78bis@gmail.com>
2020-07-04 19:19:07 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udemy-iOS-I_Am_Poor
+//
+//  Created by Alex on 04/07/2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
